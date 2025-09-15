fca20b83ffeb3d8f3927b122b9252b20154f6c7d
Agam1025 <naufalagam59070@gmail.com>
2025-09-15 14:17:03 +0700

    Commit Ulang

--- football_news/settings.py
+++ football_news/settings.py
@@ -34,7 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "naufal-agam-footbalnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://pbp.cs.ui.ac.id/naufal.agam/footbalnews"
+    "https://naufal-agam-footbalnews.pbp.cs.ui.ac.id"
 ]
 
 
